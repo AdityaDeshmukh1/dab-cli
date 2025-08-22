87508c32df47b06e9c88c99240f109682fd9f046
Aditya Deshmukh <adityad1405@gmail.com>
2025-08-23 01:50:26 +0530

    refactor: remove urfave-cli methods and use only bubbletea

--- main.go
+++ main.go
@@ -1,34 +1,9 @@
 package main
 
 import (
-	"log"
-	"os"
-
-	"github.com/urfave/cli/v2"
 	"github.com/adityadeshmukh1/dab-cli/cmd"
 )
 
 func main() {
-	app := &cli.App {
-		Name: "dab-cli",
-		Usage: "A CLI music player powered by DAB API",
-		Commands: []*cli.Command {
-			cmd.LoginCommand(),
-			cmd.SearchCommand(),
-			cmd.PlayCommand(),
-			cmd.DownloadCommand(),
-			 {
-			 	 Name: "tui",
-			 	 Usage: "Run the interactive TUI", 
-			 	 Action: func(c *cli.Context) error {
-			 	 	 cmd.RunTUI()
-			 	 	 return nil
-			 	 },
-			 },
-		},
-	}
-
-	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
-	}
+	cmd.RunTUI()
 }
