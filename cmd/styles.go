52a0955c5ecee0b19aba27fce305939dd1c54d32
Aditya Deshmukh <adityad1405@gmail.com>
2025-09-13 18:33:33 +0530

    refactor: move styles to styles.go

---
+++ cmd/styles.go
@@ -0,0 +1,26 @@
+package cmd
+
+import "github.com/charmbracelet/lipgloss"
+
+var (
+	titleStyle = lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#FFCC00")).
+			MarginTop(1).
+			MarginBottom(1).
+			MarginLeft(2)
+
+	itemStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#FFFFFF")).
+			PaddingLeft(4)
+
+	selectedItemStyle = lipgloss.NewStyle().
+				Bold(true).
+				Foreground(lipgloss.Color("#00FFAA")).
+				PaddingLeft(2)
+
+	helpStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#777777")).
+			MarginTop(1).
+			MarginLeft(2)
+)
--- cmd/tui.go
+++ cmd/tui.go
@@ -15,29 +15,6 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-var (
-	titleStyle = lipgloss.NewStyle().
-			Bold(true).
-			Foreground(lipgloss.Color("#FFCC00")).
-			MarginTop(1).
-			MarginBottom(1).
-			MarginLeft(2)
-
-	itemStyle = lipgloss.NewStyle().
-			Foreground(lipgloss.Color("#FFFFFF")).
-			PaddingLeft(4)
-
-	selectedItemStyle = lipgloss.NewStyle().
-				Bold(true).
-				Foreground(lipgloss.Color("#00FFAA")).
-				PaddingLeft(2)
-
-	helpStyle = lipgloss.NewStyle().
-			Foreground(lipgloss.Color("#777777")).
-			MarginTop(1).
-			MarginLeft(2)
-)
-
 type model struct {
 	choices  []string
 	cursor   int
@@ -372,4 +349,3 @@ func RunTUI() {
 		os.Exit(1)
 	}
 }
-
